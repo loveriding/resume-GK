d6f8d468fd795acaed00274167cc316be2cfe361
NamThien <nguyenthuy0914@gmail.com>
2020-05-12 01:07:46 +0700

    add feater des

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
 
 var course = [];
 
+
+
 	$(document).ready(function() {
-		for(let i = 0; i < 4; i++) {
+for(let i = 0; i < 4; i++) {
 			$.get("http://demo6370041.mockable.io/getcourses").done(function(data) {
 			data = data.data;
 	    	id = data[i].id;
 	      	name = data[i].name;
-	      	console.log(data[i]);
-	      	course.push("<tr><td>" + id + "</td><td>" + name + "</td></tr>");
+	      	//console.log(data[i]);
+	      	course.push("<tr class = 'slidedown_head'><td>" + (i + 1) + "</td><td>" + name + "</td></tr>");
 			 + $.get("http://demo6370041.mockable.io/course/"+ (i + 1) ).done(function(data) {
-			 	$("tbody").append(`${course[i]}<tr><td colspan="2"><div>${data.textbook}</div></td></tr>`)
+			 	$("tbody").append(`${course[i]}<tr class = "slidedown_none"><td colspan="2"><div>${data.textbook}</div></td></tr>`);
 		});
 
 	});
 		}
-		
+		$(document).on("click",'.slidedown_head',function(e) {
+			e.preventDefault();
+			$(this).next(".slidedown_none").toggleClass("slidedown_body");
+		});
 });
+
+
 
 
 	
