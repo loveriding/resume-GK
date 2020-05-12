5d8f93bd560e597900ac73755876dca06093ea4d
NamThien <nguyenthuy0914@gmail.com>
2020-05-12 16:20:56 +0700

    90%

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 
 var course = [];
-
-
-
+var visible = 0;
 $(document).ready(function() {
-
+	var count = 2;
 	//show course
 	printTable(0);
 	printTable(1);
-
+	showing(visible);
 	//show des and textbook.
 		$(document).on("click",'.slidedown_head',function(e) {
 			e.preventDefault();
@@ -17,23 +15,49 @@ $(document).ready(function() {
 
 
 	//UITABS selcct
-
-	$('ul.tabs li').click(function(){
-		var tab_id = $(this).attr('data-tab');
-		$('ul.tabs li').removeClass('current');
-		$('.tab-content').removeClass('current');
-		$(this).addClass('current');
-		$("#"+tab_id).addClass('current');
-	})
+	$('ul.tabs li').click(showDiv);
 
 	//pre-next
+	var tab = document.getElementsByClassName('tab-link');
+
+	$("button.pre").click(previous);
+	$("button.next").click(function() {
+		if(visible < count - 1) {
+			$('ul.tabs li:eq('+ visible +')').removeClass('current');
+			$('.tab-content').removeClass('current');
+			$(".tab-link:eq(" + (visible + 1) + ")").addClass("current");
+			$("#tab-" + (visible + 2)).addClass("current");
+			visible ++;
+			console.log(visible);
+			showing(visible);
+		}
+	})
 
 		
 
 
 });
 
+	function previous() {
+		if(visible !== 0) {
+			$('ul.tabs li:eq('+ visible +')').removeClass('current');
+			$('.tab-content').removeClass('current');
+			$(".tab-link:eq(" + (visible -1) + ")").addClass("current");
+			$("#tab-" + (visible)).addClass("current");
+			visible --;
+			showing(visible);
+		}
+	}
 
+	function showDiv(e) {
+		var tab_id = $(this).attr('data-tab');
+		visible = parseInt(tab_id.substr(4)) - 1;
+		showing(visible);
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	}
 
 
 	function printTable(stt) {
@@ -49,6 +73,10 @@ $(document).ready(function() {
 
 	});
 		}
+	}
+
+	function showing(i) {
+		$(".show-of").html(`Showing ${(i * 4) + 1} to ${(i + 1) * 4} of 8 entries`)
 	}
 
 
